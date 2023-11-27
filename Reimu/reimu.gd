773ec286c311145f2b2098dc9e32bcabf7dce453
Mr-Paradigm <paraxenoformula@gmail.com>
2023-11-26 22:24:04 -0800

    Menu

--- Reimu/reimu.gd
+++ Reimu/reimu.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+@onready var floating = $AnimatedSprite2D
 var speed := 500.0
 var shspeed := speed/2
 
+
 func _physics_process(delta):
+	floating.play("fly")
+	
 	velocity = Input.get_vector("Left","Right","Up","Down").normalized()*(shspeed if Input.is_physical_key_pressed(KEY_SHIFT) else speed)
 	move_and_slide()
