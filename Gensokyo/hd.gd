79df722e5e1004b31e5646fbfd5a4cf88b5ed695
southernscreamer32 <aidanlim192@gmail.com>
2023-11-26 17:05:04 -0800

    gaming

--- Gensokyo/hd.gd
+++ Gensokyo/hd.gd
@@ -1,14 +1,14 @@
 extends Node2D
 @export var bulletscene: PackedScene
-var list: Array[RigidBody2D]
+@export var okuu: CharacterBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start()
 
 func test():
 	var bullet = bulletscene.instantiate()
-	bullet.position = Vector2(100,100)
-	bullet.linear_velocity = Vector2(50,-10)
+	bullet.position = okuu.position + Vector2(0,100)
+	bullet.linear_velocity = Vector2(0,100)
 	add_child(bullet)
 
 func _on_timer_timeout():
--- Reimu/reimu.gd
+++ Reimu/reimu.gd
@@ -4,8 +4,5 @@ var speed := 500.0
 var shspeed := speed/2
 
 func _physics_process(delta):
-	movement()
-
-func movement():
 	velocity = Input.get_vector("Left","Right","Up","Down").normalized()*(shspeed if Input.is_physical_key_pressed(KEY_SHIFT) else speed)
 	move_and_slide()
